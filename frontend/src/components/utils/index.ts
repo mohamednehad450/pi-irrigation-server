cd2ea66b9d5f5ee22a17ab7cbf52b875fd9ca531
Mohamed Nehad <mohammednehad550@gmail.com>
2021-04-10 16:47:58 +0200

    feat: Update utils

--- frontend/src/components/utils/index.ts
+++ frontend/src/components/utils/index.ts
@@ -32,3 +32,28 @@ export const validateUsername = (username: string): boolean => {
     const re = /^(\w|[@_*+-]){1,150}$/
     return re.test(username)
 }
+
+
+interface Id {
+    id: string | number
+}
+
+export function removeFromArray<T extends Id>(arr: T[], id: T['id']) {
+    return arr.filter((item) => id !== item.id)
+}
+export function replaceFromArray<T extends Id>(arr: T[], item: T) {
+    const i = arr.findIndex((old) => old.id === item.id)
+    if (i >= 0) {
+        arr[i] = item
+        return [...arr]
+    }
+    throw Error('Item doesn\'t exist in in the Array.')
+}
+export function updateItemInArray<T extends Id>(arr: T[], id: T['id'], updater: (arg: T) => T) {
+    const i = arr.findIndex((item) => item.id === id)
+    if (i >= 0) {
+        arr[i] = updater(arr[i])
+        return [...arr]
+    }
+    throw Error('Item doesn\'t exist in in the Array.')
+}
